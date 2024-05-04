7bbe42ef5c9f24160e3c43ad414373704e42a008
Adam Rackis <adamrackis@hotmail.com>
2024-05-03 21:43:46 -0500

    data displaying

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
+	import type { Book } from '$lib';
 	import { onMount } from 'svelte';
+
+	let books: Book[] = [];
 
 	onMount(() => {
 		fetch('/api/books')
 			.then((resp) => resp.json())
-			.then((books) => {
-				debugger;
+			.then((booksResp) => {
+				books = booksResp;
 			});
 	});
 </script>
+
+<div>
+	{#each books as book}
+		<span>{book.title}</span>
+	{/each}
+</div>
